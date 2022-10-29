ca36857f40f1828501b23131a43ddeb17e868663
Arman777777 <arman.manukyan4.y@gmail.com>
2022-10-29 22:32:17 +0400

    new project
    this is a movie website that you can search with using the omdb api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [movie, setMovie] = useState("");
+
+  const getMovies = async (title) => {
+    await axios
+      .get(`?apikey=${process.env.REACT_APP_API_KEY}&s=${title}`)
+      .then((res) => {
+        setMovie(res.data);
+        console.log(res.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getMovies("batman");
+  }, []);
+
+  return <div>App</div>;
+};
 
 export default App;
