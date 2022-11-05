52c84cf7a7e0aba815efbf587f8488f06da6c715
Arman777777 <arman.manukyan4.y@gmail.com>
2022-11-05 06:08:26 +0400

    Navbar done ✅

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,49 +32,73 @@ const Navbar = () => {
   };
 
   return (
-    <div className="sm:flex p-[5px] items-center ">
-      <div className="flex-[0.20]">
-        <div className="flex justify-between items-center">
+    <div className="">
+      <div className="sm:flex p-[5px] items-center m-[auto]">
+        <div className="flex-[0.20]">
+          <div className="flex justify-between items-center">
+            <FontAwesomeIcon
+              className="flex-[0.2] sm:hidden text-[20px] cursor-pointer pl-[5px]"
+              icon={faBookmark}
+            />
+            <img
+              src="https://images.ctfassets.net/4cd45et68cgf/7LrExJ6PAj6MSIPkDyCO86/542b1dfabbf3959908f69be546879952/Netflix-Brand-Logo.png?w=684&h=456"
+              alt=""
+              className="w-[180px] max-w-[180px] flex-[0.6] cursor-pointer"
+            />
+            <FontAwesomeIcon
+              className="flex-[0.2] sm:hidden text-[20px] cursor-pointer pr-[5px]"
+              icon={faUser}
+            />
+          </div>
+        </div>
+        <form
+          className="flex-[0.70] mx-[8px] 2xl:hidden"
+          onSubmit={submitSearch}
+        >
+          <div className="bg-[rgba(236,240,243,.2)] backdrop-blur-[5px] rounded-[50px] p-[12px] px-[20px] shadow-[rgba(149,157,165,0.2)_0px_8px_24px] ">
+            <button className="bg-transparent" type="submit">
+              <FontAwesomeIcon
+                icon={faMagnifyingGlass}
+                className="text-[#A3A6AF]"
+              />
+            </button>
+            <input
+              className="bg-transparent outline-none ml-[10px]"
+              type="text"
+              placeholder="Search"
+              onChange={(e) => setInput(e.target.value)}
+            />
+          </div>
+        </form>
+        <div className="flex 2xl:flex-[0.9] flex-[0.1] justify-end mmax:hidden items-center gap-[30px] mx-[10px]">
+          <form
+            className="flex-[0.90] mx-[8px] sxl:hidden max-w-[600px]"
+            onSubmit={submitSearch}
+          >
+            <div className="bg-[rgba(236,240,243,.2)] backdrop-blur-[5px] rounded-[50px] p-[12px] px-[20px] shadow-[rgba(149,157,165,0.2)_0px_8px_24px] ">
+              <button className="bg-transparent" type="submit">
+                <FontAwesomeIcon
+                  icon={faMagnifyingGlass}
+                  className="text-[#A3A6AF]"
+                />
+              </button>
+              <input
+                className="bg-transparent outline-none ml-[10px]"
+                type="text"
+                placeholder="Search"
+                onChange={(e) => setInput(e.target.value)}
+              />
+            </div>
+          </form>
           <FontAwesomeIcon
-            className="flex-[0.2] sm:hidden text-[20px] cursor-pointer pl-[5px]"
+            className="flex-[0.01] text-[20px] cursor-pointer"
             icon={faBookmark}
           />
-          <img
-            src="https://images.ctfassets.net/4cd45et68cgf/7LrExJ6PAj6MSIPkDyCO86/542b1dfabbf3959908f69be546879952/Netflix-Brand-Logo.png?w=684&h=456"
-            alt=""
-            className="w-[180px] max-w-[180px] flex-[0.6] cursor-pointer"
-          />
           <FontAwesomeIcon
-            className="flex-[0.2] sm:hidden text-[20px] cursor-pointer pr-[5px]"
+            className="flex-[0.01] text-[20px] cursor-pointer 2xl:mr-[10px]"
             icon={faUser}
           />
         </div>
-      </div>
-      <form className="flex-[0.65] mx-[8px]" onSubmit={submitSearch}>
-        <div className="bg-[rgba(236,240,243,.2)] backdrop-blur-[5px] rounded-[50px] p-[10px] shadow-[rgba(149,157,165,0.2)_0px_8px_24px] ">
-          <button className="bg-transparent" type="submit">
-            <FontAwesomeIcon
-              icon={faMagnifyingGlass}
-              className="text-[#A3A6AF]"
-            />
-          </button>
-          <input
-            className="bg-transparent outline-none ml-[10px]"
-            type="text"
-            placeholder="Search"
-            onChange={(e) => setInput(e.target.value)}
-          />
-        </div>
-      </form>
-      <div className="flex flex-[0.15] justify-center mmax:hidden">
-        <FontAwesomeIcon
-          className="flex-[0.5] text-[20px] cursor-pointer"
-          icon={faBookmark}
-        />
-        <FontAwesomeIcon
-          className="flex-[0.5] text-[20px] cursor-pointer"
-          icon={faUser}
-        />
       </div>
     </div>
   );
