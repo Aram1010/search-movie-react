132a5c9595e652fa5ab259d4f2badaf2cd55ade8
Arman777777 <arman.manukyan4.y@gmail.com>
2022-11-08 12:08:01 +0400

    added rating runtime changed look

--- src/components/MovieDescription.jsx
+++ src/components/MovieDescription.jsx
@@ -4,62 +4,54 @@ import { faPlay } from "@fortawesome/free-solid-svg-icons";
 
 const MovieDescription = ({ movie }) => {
   return (
-    <div>
-      <h2
-        style={{ fontSize: "clamp(10px , 8vw , 60px)" }}
-        className="text-700 max-w-[350px] mx-[20px] mt-[30px]"
-      >
-        {movie ? movie.title : null}
-      </h2>
-      <div
-        className="absolute bottom-0 smax:left-[50%] smax:translate-x-[-50%] 
-                  w-[90%] max-w-[800px] h-[40%] min-h-[300px] bg-[rgba(236,240,243,.2)] 
+    <div
+      className="absolute bottom-0 smax:left-[50%] smax:translate-x-[-50%] 
+                  w-[90%] smin:max-w-[600px] max-w-[800px] h-[40%] min-h-[300px] bg-[rgba(236,240,243,.2)] 
                   shadow-[rgba(149,157,165,0.2)_0px_8px_24px] backdrop-blur-[5px] rounded-tr-[50px] 
                   smax:rounded-tl-[50px] p-[20px] pb-[30px] "
-      >
-        <div className="grid grid-cols-2 grid-rows-2 gap-[30px] h-[90%] absolute">
-          <span>
-            <h2 className="smin:text-[20px]">Category</h2>
-            <ul className="smin:text-[16px]">
-              {movie.genres[0]
-                ? movie.genres.map((genre, ind) => (
-                    <span>{(ind ? ", " : "") + genre.name}</span>
+    >
+      <div className="grid grid-cols-2 grid-rows-2 gap-[30px] h-[90%] absolute">
+        <span>
+          <h2 className="smin:text-[20px]">Category</h2>
+          <ul className="smin:text-[16px]">
+            {movie.genres[0]
+              ? movie.genres.map((genre, ind) => (
+                  <span>{(ind ? ", " : "") + genre.name}</span>
+                ))
+              : null}
+          </ul>
+        </span>
+        <span>
+          <h2 className="smin:text-[20px]">Storyline</h2>
+          <p className="max-h-[100px] max-w-[250px] text-[15px] smin:text-[16px] overflow-y-scroll scrollbar-hide">
+            {movie.overview}
+          </p>
+        </span>
+        <span>
+          <h2 className="smin:text-[20px]">Starring</h2>
+          <ul className="max-h-[100px] smin:text-[16px] overflow-y-scroll scrollbar-hide">
+            {movie.credits
+              ? movie.credits.cast
+                  .slice(0, 6)
+                  .map((cast, ind) => (
+                    <span>{(ind ? ", " : "") + cast.name}</span>
                   ))
-                : null}
-            </ul>
-          </span>
-          <span>
-            <h2 className="smin:text-[20px]">Storyline</h2>
-            <p className="max-h-[100px] max-w-[300px] text-[15px] smin:text-[16px] overflow-y-scroll scrollbar-hide">
-              {movie.overview}
-            </p>
-          </span>
-          <span>
-            <h2 className="smin:text-[20px]">Starring</h2>
-            <ul className="max-h-[100px] smin:text-[16px] overflow-y-scroll scrollbar-hide">
-              {movie.credits
-                ? movie.credits.cast
-                    .slice(0, 6)
-                    .map((cast, ind) => (
-                      <span>{(ind ? ", " : "") + cast.name}</span>
-                    ))
-                : null}
-              <p>...</p>
-            </ul>
-          </span>
-          <span>
-            <button
-              type="button"
-              className="flex items-center bg-[#ffff] py-[12px] px-[18px] smin:text-[18px] smin:px-[20px] smin:py-[12px] 
+              : null}
+            <p>...</p>
+          </ul>
+        </span>
+        <span>
+          <button
+            type="button"
+            className="flex items-center bg-[#ffff] py-[12px] px-[18px] smin:text-[18px] smin:px-[20px] smin:py-[12px] 
                   text-[#222] shadow-[rgba(100,100,111,0.9)_0px_7px_29px_0px]
-                  rounded-[50px] absolute bottom-[1rem] right-[1rem] 
+                  rounded-[50px] absolute bottom-[5rem] right-[1rem] 
                   hover:scale-[1.05] transition duration-100 ease-in-out"
-            >
-              <FontAwesomeIcon icon={faPlay} />
-              <p className="ml-[10px] text-[14px]">Play Trailer</p>
-            </button>
-          </span>
-        </div>
+          >
+            <FontAwesomeIcon icon={faPlay} />
+            <p className="ml-[10px] text-[14px]">Play Trailer</p>
+          </button>
+        </span>
       </div>
     </div>
   );
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import MovieDescription from "../components/MovieDescription";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const IMAGE_PATH = "https://www.themoviedb.org/t/p/w780";
 
@@ -13,7 +15,14 @@ const Movie = () => {
     setMovie(location.state);
   }, [location]);
 
-  console.log(movie);
+  const mToh = (total_minutes) => {
+    const hours = Math.floor(total_minutes / 60);
+    const minutes = total_minutes % 60;
+
+    return (
+      <p className="ml-[20px]">{`${hours}h ${minutes}m`}</p>
+    )
+  }
 
   return (
     <div className="text-[#ffff] h-[100%]">
@@ -27,7 +36,33 @@ const Movie = () => {
         }
         className="fixed top-0 left-0 w-[100%] h-[100vh] smax:bg-cover smax:bg-no-repeat bg-center -z-50"
       ></div>
+      <div className="mx-[20px]">
+        <h2
+          style={{ fontSize: "clamp(10px , 8vw , 50px)" }}
+          className="text-700 max-w-[500px] mt-[30px]"
+        >
+          {movie ? movie.title : null}
+        </h2>
+        <div className="flex items-center mt-[10px]">
+          <FontAwesomeIcon className="text-[#F2BB2E]" icon={faStar} />
+          <p className="ml-[10px]">
+            {movie
+              ? `${Math.round(movie.vote_average * 100) / 100} | ${
+                  movie.vote_count
+                }`
+              : null}
+          </p>
+          {movie ? mToh(movie.runtime) : null}
+        </div>
+      </div>
       {movie ? <MovieDescription movie={movie} /> : null}
+      <div
+        className="absolute bottom-0 left-0 w-[100%] h-[10%] "
+        style={{
+          backgroundImage:
+            "linear-gradient(180deg,transparent,rgba(25, 25, 50, 0.61),#1B2230)",
+        }}
+      ></div>
     </div>
   );
 };
