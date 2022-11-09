abc03d60a737d48f309a16c143033545e3a94a00
Arman777777 <arman.manukyan4.y@gmail.com>
2022-11-09 15:01:17 +0400

    little tweaks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import React, { useState ,  useEffect } from "react";
 import axios from "../utils/axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { DataLayerValue } from "../context/DataLayer";
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const getWindowSize = () => {
   const { innerWidth } = window;
@@ -68,15 +67,17 @@ const Navbar = () => {
     <div className="">
       <div className="flex p-[5px] items-center m-[auto] justify-between smax:mt-[20px]">
         <div className="flex-[0.50]">
-          <img
-            src={
-              windowSize.innerWidth > 900
-                ? "https://images.ctfassets.net/4cd45et68cgf/7LrExJ6PAj6MSIPkDyCO86/542b1dfabbf3959908f69be546879952/Netflix-Brand-Logo.png?w=684&h=456"
-                : "https://1000logos.net/wp-content/uploads/2017/05/Netflix-Logo-2006.png"
-            }
-            alt=""
-            className="w-[180px] max-w-[180px] max-h-[107px] cursor-pointer smax:w-[90px]"
-          />
+          <NavLink to="/">
+            <img
+              src={
+                windowSize.innerWidth > 900
+                  ? "https://images.ctfassets.net/4cd45et68cgf/7LrExJ6PAj6MSIPkDyCO86/542b1dfabbf3959908f69be546879952/Netflix-Brand-Logo.png?w=684&h=456"
+                  : "https://1000logos.net/wp-content/uploads/2017/05/Netflix-Logo-2006.png"
+              }
+              alt=""
+              className="w-[180px] max-w-[180px] max-h-[107px] cursor-pointer smax:w-[90px] relative z-[100]"
+            />
+          </NavLink>
         </div>
         <form
           className="flex-[0] mx-[8px] w-[100px]"
@@ -98,8 +99,8 @@ const Navbar = () => {
         >
           <div
             className="flex bg-[rgba(236,240,243,.2)] backdrop-blur-[5px] 
-              rounded-[50px] p-[12px] px-[20px] shadow-[rgba(149,157,165,0.2)_0px_8px_24px] "
-              id="search"
+              rounded-[50px] p-[12px] px-[17px] shadow-[rgba(149,157,165,0.2)_0px_8px_24px] "
+            id="search"
           >
             <button
               className="bg-transparent"
